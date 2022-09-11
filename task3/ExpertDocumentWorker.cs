bf2b1ac35c62225087a52dea95696aeaf9100b13
Ivan <89978208+IvanGalk1n@users.noreply.github.com>
2022-09-11 12:33:12 +0300

    Add files via upload

---
+++ task3/ExpertDocumentWorker.cs
@@ -0,0 +1,9 @@
+﻿namespace task3;
+
+public class ExpertDocumentWorker : ProDocumentWorker
+{
+    public override void SaveDocument()
+    {
+        Console.WriteLine("Документ сохранен в новом формате");
+    }
+}
---
+++ task3/Program.cs
@@ -0,0 +1,32 @@
+﻿using task3;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        const string proKey    = "1234567890";
+        const string expertKey = "0987654321";
+        
+        Console.WriteLine("Введите ключ активации Pro или Expert версии");
+        Console.WriteLine("Для использования обычной версии нажмите Enter");
+
+        string? key = Console.ReadLine();
+
+        if (key == proKey)
+        {
+            DocumentWorker pro = new ProDocumentWorker();
+            Console.WriteLine("Вас приветствует Pro-версия DocumentWorker");
+        }
+        else if (key == expertKey)
+        {
+            DocumentWorker exp = new ExpertDocumentWorker();
+            Console.WriteLine("Вас приветствует Expert-версия DocumentWorker");
+            exp.SaveDocument();
+        }
+        else
+        {
+            DocumentWorker common = new DocumentWorker();
+            Console.WriteLine("Вас приветствует обычная версия DocumentWorker");
+        }
+    }
+}
